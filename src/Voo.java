b82888dd1150e8ada897d7d5576c09a85268163a
Andrade <vinicius_andrade2010@hotmail.com>
2024-09-29 16:40:31 -0300

    setup:

---
+++ src/Voo.java
@@ -0,0 +1,9 @@
+public class Voo {
+    int numeroVoo;
+    int destino;
+
+    public Voo(int numeroVoo, int destino) {
+        this.numeroVoo = numeroVoo;
+        this.destino = destino;
+    }
+}
---
+++ src/estruturas/Queue/Queue.java
@@ -0,0 +1,162 @@
+package estruturas.Queue;
+
+import java.util.EmptyStackException;
+
+import static estruturas.ShallowOrDeepCopy.ShallowOrDeepCopy.verifyAndCopy;
+
+public class Queue<X> implements Cloneable {
+
+    private Object[] elemento;
+    private final int tamanhoInicial;
+    private int ultimo = -1;
+
+    public Queue() {
+        this.tamanhoInicial = 10;
+        this.elemento = new Object[tamanhoInicial];
+    }
+
+    public Queue(int tamanhoInicial) {
+        if (tamanhoInicial <= 0) throw new IllegalArgumentException("Tamanho inválido");
+
+        this.elemento = new Object[tamanhoInicial];
+        this.tamanhoInicial = tamanhoInicial;
+    }
+
+    public void enqueue(X x) {
+        if (x == null) throw new IllegalArgumentException("Elemento nulo");
+
+        if (this.isFull()) this.redimensionarParaCima();
+
+        this.ultimo++;
+        this.elemento[this.ultimo] = verifyAndCopy(x);
+    }
+
+    @SuppressWarnings("unchecked")
+    public X peek() {
+        if (this.isEmpty()) throw new EmptyStackException();
+
+        X ret;
+
+        ret = (X) verifyAndCopy(this.elemento[0]); // FIFO: Retorna o primeiro elemento inserido
+
+        return ret;
+    }
+
+    @SuppressWarnings("unchecked")
+    public X dequeue() {
+        if (this.isEmpty()) throw new EmptyStackException();
+
+        X ret = (X) verifyAndCopy(this.elemento[0]);
+
+        for (int i = 0; i < this.ultimo; i++)
+            this.elemento[i] = this.elemento[i + 1];
+
+        this.elemento[this.ultimo] = null;
+        this.ultimo--;
+
+        if (this.elemento.length > this.tamanhoInicial &&
+                this.ultimo + 1 <= Math.round((float) this.elemento.length / 4))
+            this.redimensionarParaBaixo();
+
+        return ret;
+    }
+
+    private void redimensionarParaBaixo() {
+
+        Object[] novo = new Object[(int) Math.ceil((float) this.elemento.length / 2)];
+
+        for (int i = 0; i < novo.length; i++)
+            novo[i] = this.elemento[i];
+
+        this.elemento = novo;
+    }
+
+    private void redimensionarParaCima() {
+        Object[] novo = new Object[(int) Math.ceil((float) this.elemento.length * 2)];
+
+        for (int i = 0; i < this.elemento.length; i++)
+            novo[i] = this.elemento[i];
+
+        this.elemento = novo;
+    }
+
+    public boolean isEmpty() {
+        return this.ultimo == -1;
+    }
+
+    public boolean isFull() {
+        return this.ultimo + 1 == this.elemento.length;
+    }
+
+    public String toArray() {
+        if (this.isEmpty()) return "[]";
+        StringBuilder ret = new StringBuilder("[");
+        for (int i = 0; i < this.ultimo; i++)
+            ret.append(this.elemento[i]).append(", ");
+        ret.append(this.elemento[this.ultimo]).append("]");
+        return ret.toString();
+    }
+
+    // Construtor de cópia
+    public Queue(Queue<X> modelo) {
+        if (modelo == null) throw new IllegalArgumentException("Modelo ausente");
+
+        this.tamanhoInicial = modelo.tamanhoInicial;
+        this.ultimo = modelo.ultimo;
+
+        this.elemento = new Object[this.tamanhoInicial];
+
+        for (int i = 0; i < this.ultimo; i++)
+            this.elemento[i] = modelo.elemento[i];
+    }
+
+    @Override
+    public Object clone() {
+        Queue<X> clone = null;
+        try {
+            clone = new Queue<X>(this);
+        } catch (Exception ignored) {
+        }
+        return clone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (this.getClass() != obj.getClass()) return false;
+
+        Queue<?> other = (Queue<?>) obj;
+
+        if (this.tamanhoInicial != other.tamanhoInicial) return false;
+        if (this.ultimo != other.ultimo) return false;
+
+        for (int i = 0; i < this.ultimo; i++)
+            if (!this.elemento[i].equals(other.elemento[i]))
+                return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int hash = 1;
+
+        hash *= prime + Integer.hashCode(this.tamanhoInicial);
+        hash *= prime + Integer.hashCode(this.ultimo);
+
+        for (int i = 0; i < this.ultimo; i++)
+            hash *= prime + this.elemento[i].hashCode();
+
+        if (hash < 0) hash = -hash;
+
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        if (this.isEmpty()) return "[]";
+        return "[" + this.elemento[0] + "]";
+    }
+}
